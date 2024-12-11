f3ce4a2fa34c094d26cc807f5f1b381cf4018a0d
cleverice007 <cleverice90128@gmail.com>
2024-12-11 15:37:35 +0800

    update hw3_20.py
    1. using stochastic gradient descent to implement logistic regression

--- HW3/hw3_20.py
+++ HW3/hw3_20.py
@@ -1,8 +1,8 @@
+#read file ,split data into X and Y
+# add a column of 1 to X
 
 import numpy as np
-# read hw3_test.dat & hw3_train.dat
-# split data into labels and X
-# add a column of 1 to X
+
 def read_file(file_name):
     with open(file_name, 'r') as f:
         lines = f.readlines()
@@ -13,33 +13,30 @@ def read_file(file_name):
         X = data[:, :-1]
         Y = data[:, -1]
         X = np.insert(X, 0, 1, axis=1)
-    return X , Y
+    return X, Y
 
 # learning rate = 0.001, number of iterations = 2000
-#  calculate the gradient and update weights
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-# q19：leanring rate = 0.01
-def update_weights(X, Y, learning_rate=0.001, num_iterations=2000):
+# update weights using stochastic gradient descent
+# update weights one sample at a time
+# formula of gradient: (sigmoid(w^T * x) - y) * x
+
+def update_weights(X, Y, learning_rate=0.001, num_epochs=2):
     # Initialize weights to zeros
     w = np.zeros(X.shape[1])
     
-    # Perform gradient descent
-    for _ in range(num_iterations):
-        # Calculate predictions using sigmoid
-        predictions = sigmoid(np.dot(X, w))
-        
-        # Compute the gradient
-        gradient = np.dot(X.T, (predictions - Y)) / len(Y)
-        
-        # Update weights
-        w -= learning_rate * gradient
-    
+    # Perform stochastic gradient descent
+    # run iterations for num_epochs
+    for _ in range(num_epochs):
+        for i in range(len(Y)):
+            
+            gradient = (sigmoid(np.dot(X[i], w)) - Y[i]) * X[i]
+            # Update weights
+            w -= learning_rate * gradient
     return w
-
-
 
 # calculate mean 0/1 error
 
@@ -53,17 +50,13 @@ def calculate_error(X, Y, w):
     
     return error_rate
 
-# calculate weights using the training data
-# calculate eout using the test data
 if __name__ == "__main__":
     train_file = "hw3_train.dat"
     test_file = "hw3_test.dat"
     
     X_train, Y_train = read_file(train_file)
     X_test, Y_test = read_file(test_file)
-    
     w = update_weights(X_train, Y_train)
     error_rate = calculate_error(X_test, Y_test, w)
     print("Weights: ", w)
     print("Error rate: ", error_rate)
-
